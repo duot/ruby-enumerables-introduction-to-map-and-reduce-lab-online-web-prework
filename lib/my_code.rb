6459322c6805a42411d62350f03c08688a8968c8
optimistic-writer-7178 <duot.jim@gmail.com>
2019-07-19 03:13:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,7 +32,7 @@ def reduce_to_all_true arr
     if !val
       some_falsey = true
     end
-  } ? false : true # if any is falsey, then not all are true
+    } ? false : true # if any is falsey, then not all are true
 end
 
 def reduce_to_any_true arr
